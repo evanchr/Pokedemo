2c9e23ce5107599e890d3e83e130d28b77879f28
Evan Charrier <evanc378549@gmail.com>
2024-10-19 18:03:59 +0200

    tous les types dans le même fichier

--- src/app/api-pokemon.service.ts
+++ src/app/api-pokemon.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IPokemon, PokemonDetails, PokemonsList } from './model';
+import { PokemonDetails, PokemonsList } from './pokemon';
+
+const apiUrl = 'https://pokeapi.co/api/v2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiPokemonService {
-  apiUrl = 'https://pokeapi.co/api/v2';
 
   constructor(private http: HttpClient) { 
   }
 
   getPokemonsList(): Observable<PokemonsList>{
-    return this.http.get<PokemonsList>(`${this.apiUrl}/pokemon?limit=1025&offset=0`)
+    return this.http.get<PokemonsList>(`${apiUrl}/pokemon?limit=1025&offset=0`)
   }
 
   getPokemonDetailsById(id: string): Observable<PokemonDetails> {
-    return this.http.get<PokemonDetails>(`${this.apiUrl}/pokemon/${id}`)
+    return this.http.get<PokemonDetails>(`${apiUrl}/pokemon/${id}`)
   }
 }
